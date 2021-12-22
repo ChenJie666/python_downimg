91fe06566b960dd311141be715e73819456aa0fc
cj <792965772@qq.com>
2021-12-22 19:13:28 +0800

    修改当前文件位置的判断逻辑

--- com/cj/downimg/downimg-gui.py
+++ com/cj/downimg/downimg-gui.py
@@ -11,7 +11,6 @@ from PIL.ImageTk import PhotoImage
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tkinter import Tk, constants, Scrollbar, Canvas, BooleanVar, Checkbutton, Label, Button, Frame, messagebox
 from threading import Thread
-import os, sys
 
 
 # from multiprocessing import cpu_count
@@ -222,7 +221,7 @@ def start_gui():
         inv_btn.grid(row=0, column=2, sticky=constants.W, padx="2")
 
         global dir_path
-        dir_path = askdirectory(initialdir=os.path.abspath('.'))
+        dir_path = askdirectory(initialdir=path.abspath('.'))
         # dir_path = askdirectory(initialdir=os.path.realpath(sys.argv[0]))
         # dir_path = askdirectory(initialdir=path.dirname(__file__))
 
